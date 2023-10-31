5721222f94f1ee6d1238e584ad580bea3bb4d770
BuzzHeng <leeyuanheng16@gmail.com>
2023-10-31 09:53:48 +0800

    Eureka Server and client for pricing service
    Implemented Eureka Server and convert to microservices for pricing service

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (currency, price, vehicleId) VALUES ('USD', 100000, 1);
+INSERT INTO price (currency, price, vehicleId) VALUES ('SGD', 140000, 2);
+INSERT INTO price (currency, price, vehicleId) VALUES ('RM', 490000, 3);
+INSERT INTO price (currency, price, vehicleId) VALUES ('YEN', 15400000, 4);
+INSERT INTO price (currency, price, vehicleId) VALUES ('RUPEE', 8470000, 5);
